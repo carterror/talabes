5e3dea2f5004514d3b84e68f6b3682a998096a48
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2022-03-21 16:20:14 -0400

    Update PaypalPaymentController.php

--- app/Http/Controllers/PaypalPaymentController.php
+++ app/Http/Controllers/PaypalPaymentController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 
+use \PayPal\Api\Amount;
+use \PayPal\Api\Details;
+use \PayPal\Api\Payment;
+use \PayPal\Api\PaymentExecution;
+use \PayPal\Api\Transaction;
+
 
 class PayPalPaymentController extends Controller
 {
@@ -51,9 +57,7 @@ class PayPalPaymentController extends Controller
         // 4. Make a Create Call and print the values
         try {
             $payment->create($this->apiContext);
-            echo $payment;
-
-            echo "\n\nRedirect user to approval_url: " . $payment->getApprovalLink() . "\n";
+            return redirect()->away($payment->getApprovalLink());
         }
         catch (\PayPal\Exception\PayPalConnectionException $ex) {
             // This will print the detailed information on the exception.
@@ -63,6 +67,80 @@ class PayPalPaymentController extends Controller
 
     }
 
+
+    public function paymentCancel()
+    {
+
+    return "Cancelado";
     
+    }
+
+    public function paymentSuccess(Request $request)
+    {
+
+        return dd($request->all());
+        // if (isset($_GET['success']) && $_GET['success'] == 'true') {
+        //     // Get the payment Object by passing paymentId payment id was previously stored in session in CreatePaymentUsingPayPal.php
+            
+        //         $paymentId = $_GET['paymentId'];
+        //         $payment = Payment::get($paymentId, $this->apiContext);
+        //     // Payment Execute
+        //     // PaymentExecution object includes information necessary to execute a PayPal account payment. The payer_id is added to the request query parameters when the user is redirected from paypal back to your site
+            
+        //         $execution = new PaymentExecution();
+        //         $execution->setPayerId($_GET['PayerID']);
+        //     // Optional Changes to Amount
+        //     // If you wish to update the amount that you wish to charge the customer, based on the shipping address or any other reason, you could do that by passing the transaction object with just amount field in it. Here is the example on how we changed the shipping to $1 more than before.
+            
+        //         $transaction = new Transaction();
+        //         $amount = new Amount();
+        //         $details = new Details();
+            
+        //         $details->setShipping(2.2)
+        //             ->setTax(1.3)
+        //             ->setSubtotal(17.50);
+            
+        //         $amount->setCurrency('USD');
+        //         $amount->setTotal(21);
+        //         $amount->setDetails($details);
+        //         $transaction->setAmount($amount);
+        //     // Add the above transaction object inside our Execution object.
+            
+        //         $execution->addTransaction($transaction);
+            
+        //         try {
+        //     // Execute the payment (See bootstrap.php for more on ApiContext)
+            
+        //             $result = $payment->execute($execution, $this->apiContext);
+        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            
+        //             ResultPrinter::printResult("Executed Payment", "Payment", $payment->getId(), $execution, $result);
+            
+        //             try {
+        //                 $payment = Payment::get($paymentId, $this->apiContext);
+        //             } catch (Exception $ex) {
+        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            
+        //                 ResultPrinter::printError("Get Payment", "Payment", null, null, $ex);
+        //                 exit(1);
+        //             }
+        //         } catch (Exception $ex) {
+        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            
+        //             ResultPrinter::printError("Executed Payment", "Payment", null, null, $ex);
+        //             exit(1);
+        //         }
+        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            
+        //         ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
+            
+        //         return $payment;
+        //     } else {
+        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            
+        //         ResultPrinter::printResult("User Cancelled the Approval", null);
+        //         exit;
+        //     }
+    }
 
 }
