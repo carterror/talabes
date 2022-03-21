fedc2f14031a6ea1e01d2e3c6fc4ddec5a2c7567
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2022-03-21 16:46:52 -0400

    Update PaypalPaymentController.php

--- app/Http/Controllers/PaypalPaymentController.php
+++ app/Http/Controllers/PaypalPaymentController.php
@@ -86,72 +86,72 @@ class PayPalPaymentController extends Controller
     public function paymentSuccess(Request $request)
     {
 
-        $paymentId = $request->paymentId;
-        $token = $request->token;
-        $PayerID = $request->PayerID;
-        return $paymentId. " ". $token  . " ".$PayerID;
-        // if (isset($_GET['success']) && $_GET['success'] == 'true') {
-        //     // Get the payment Object by passing paymentId payment id was previously stored in session in CreatePaymentUsingPayPal.php
+        // $paymentId = $request->paymentId;
+        // $token = $request->token;
+        // $PayerID = $request->PayerID;
+        // return $paymentId. " ". $token  . " ".$PayerID;
+        if (isset($_GET['success']) && $_GET['success'] == 'true') {
+            // Get the payment Object by passing paymentId payment id was previously stored in session in CreatePaymentUsingPayPal.php
             
-        //         $paymentId = $_GET['paymentId'];
-        //         $payment = Payment::get($paymentId, $this->apiContext);
-        //     // Payment Execute
-        //     // PaymentExecution object includes information necessary to execute a PayPal account payment. The payer_id is added to the request query parameters when the user is redirected from paypal back to your site
+                $paymentId = $_GET['paymentId'];
+                $payment = Payment::get($paymentId, $this->apiContext);
+            // Payment Execute
+            // PaymentExecution object includes information necessary to execute a PayPal account payment. The payer_id is added to the request query parameters when the user is redirected from paypal back to your site
             
-        //         $execution = new PaymentExecution();
-        //         $execution->setPayerId($_GET['PayerID']);
-        //     // Optional Changes to Amount
-        //     // If you wish to update the amount that you wish to charge the customer, based on the shipping address or any other reason, you could do that by passing the transaction object with just amount field in it. Here is the example on how we changed the shipping to $1 more than before.
+                $execution = new PaymentExecution();
+                $execution->setPayerId($_GET['PayerID']);
+            // Optional Changes to Amount
+            // If you wish to update the amount that you wish to charge the customer, based on the shipping address or any other reason, you could do that by passing the transaction object with just amount field in it. Here is the example on how we changed the shipping to $1 more than before.
             
-        //         $transaction = new Transaction();
-        //         $amount = new Amount();
-        //         $details = new Details();
+                $transaction = new Transaction();
+                $amount = new Amount();
+                $details = new Details();
             
-        //         $details->setShipping(2.2)
-        //             ->setTax(1.3)
-        //             ->setSubtotal(17.50);
+                $details->setShipping(2.2)
+                    ->setTax(1.3)
+                    ->setSubtotal(17.50);
             
-        //         $amount->setCurrency('USD');
-        //         $amount->setTotal(21);
-        //         $amount->setDetails($details);
-        //         $transaction->setAmount($amount);
-        //     // Add the above transaction object inside our Execution object.
+                $amount->setCurrency('USD');
+                $amount->setTotal(21);
+                $amount->setDetails($details);
+                $transaction->setAmount($amount);
+            // Add the above transaction object inside our Execution object.
             
-        //         $execution->addTransaction($transaction);
+                $execution->addTransaction($transaction);
             
-        //         try {
-        //     // Execute the payment (See bootstrap.php for more on ApiContext)
+                try {
+            // Execute the payment (See bootstrap.php for more on ApiContext)
             
-        //             $result = $payment->execute($execution, $this->apiContext);
-        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                    $result = $payment->execute($execution, $this->apiContext);
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
             
-        //             ResultPrinter::printResult("Executed Payment", "Payment", $payment->getId(), $execution, $result);
+                    ResultPrinter::printResult("Executed Payment", "Payment", $payment->getId(), $execution, $result);
             
-        //             try {
-        //                 $payment = Payment::get($paymentId, $this->apiContext);
-        //             } catch (Exception $ex) {
-        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                    try {
+                        $payment = Payment::get($paymentId, $this->apiContext);
+                    } catch (Exception $ex) {
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
             
-        //                 ResultPrinter::printError("Get Payment", "Payment", null, null, $ex);
-        //                 exit(1);
-        //             }
-        //         } catch (Exception $ex) {
-        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                        ResultPrinter::printError("Get Payment", "Payment", null, null, $ex);
+                        exit(1);
+                    }
+                } catch (Exception $ex) {
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
             
-        //             ResultPrinter::printError("Executed Payment", "Payment", null, null, $ex);
-        //             exit(1);
-        //         }
-        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                    ResultPrinter::printError("Executed Payment", "Payment", null, null, $ex);
+                    exit(1);
+                }
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
             
-        //         ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
+                ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
             
-        //         return $payment;
-        //     } else {
-        //     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                return $payment;
+            } else {
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
             
-        //         ResultPrinter::printResult("User Cancelled the Approval", null);
-        //         exit;
-        //     }
+                ResultPrinter::printResult("User Cancelled the Approval", null);
+                exit;
+            }
     }
 
 }
