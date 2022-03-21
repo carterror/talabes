93db17a95426820496ad290a4a1dbd5f95497ca4
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2022-03-21 16:02:45 -0400

    Update PaypalPaymentController.php

--- app/Http/Controllers/PaypalPaymentController.php
+++ app/Http/Controllers/PaypalPaymentController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 
-use PayPal\Auth\OAuthTokenCredential;
-use Paypal\Rest\ApiContext;
-
 
 class PayPalPaymentController extends Controller
 {
@@ -17,9 +14,9 @@ class PayPalPaymentController extends Controller
     {
 
         $paypalconfig = Config::get("paypal");
-
-        $apiContext = new ApiContext(
-            new OAuthTokenCredential(
+        
+        $this->apiContext = new \PayPal\Rest\ApiContext(
+            new \PayPal\Auth\OAuthTokenCredential(
                 $paypalconfig["sandbox"]["certificate"],                   // ClientID
                 $paypalconfig["sandbox"]["secret"]      // ClientSecret
             )
@@ -28,7 +25,42 @@ class PayPalPaymentController extends Controller
 
     public function handlePayment()
     {
-return 123;
+        // 3. Lets try to create a Payment
+        // https://developer.paypal.com/docs/api/payments/#payment_create
+        $payer = new \PayPal\Api\Payer();
+        $payer->setPaymentMethod('paypal');
+
+        $amount = new \PayPal\Api\Amount();
+        $amount->setTotal('1.00');
+        $amount->setCurrency('USD');
+
+        $transaction = new \PayPal\Api\Transaction();
+        $transaction->setAmount($amount);
+
+        $redirectUrls = new \PayPal\Api\RedirectUrls();
+        $redirectUrls->setReturnUrl(route("success.payment"))
+            ->setCancelUrl(route("cancel.payment"));
+            
+            
+        $payment = new \PayPal\Api\Payment();
+        $payment->setIntent('sale')
+            ->setPayer($payer)
+            ->setTransactions(array($transaction))
+            ->setRedirectUrls($redirectUrls);
+
+        // 4. Make a Create Call and print the values
+        try {
+            $payment->create($this->apiContext);
+            echo $payment;
+
+            echo "\n\nRedirect user to approval_url: " . $payment->getApprovalLink() . "\n";
+        }
+        catch (\PayPal\Exception\PayPalConnectionException $ex) {
+            // This will print the detailed information on the exception.
+            //REALLY HELPFUL FOR DEBUGGING
+            echo $ex->getData();
+        }
+
     }
 
     
